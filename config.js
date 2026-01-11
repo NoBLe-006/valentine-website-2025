fe9fe9ed158c35e4a7059a74c1ba476728d37ad7
NoBLe-006 <johnathan117@icloud.com>
2026-01-10 23:39:28 -0800

    Update config.js 4

--- config.js
+++ config.js
@@ -71,13 +71,6 @@ const CONFIG = {
         floatDistance: "50px",          // How far hearts move sideways (30-70px recommended)
         bounceSpeed: "0.5s",            // Speed of bouncing animations (0.3-0.7s recommended)
         heartExplosionSize: 1.5         // Size of heart explosion effect (1.2-2.0 recommended)
-    },
-
-    // Background Music (Optional)
-    // Add your own music URL after getting proper licenses
-                 // Volume level (0.0 to 1.0)
-    }
-};
 
 // Don't modify anything below this line unless you know what you're doing
 window.VALENTINE_CONFIG = CONFIG; 
